b1f2d506cff93d38357d9f7c046defd7c050f866
Bagus Arisandi <bagus.arisandi@gli.id>
2025-01-07 10:43:35 +0700

    update body style

--- src/carouselData.js
+++ src/carouselData.js
@@ -1,7 +1,49 @@
 const Carousel = [
-{ id:1, image : "PYM_DBS-Jan-header-20250101-062129.png"},
-{ id:2, image : "PYM_DBS-Jan-header-20250101-062129.png"},
-{ id:3, image : "PYM_DBS-Jan-header-20250101-062129.png"},
+{ 
+  id: 1,
+  type: "page",
+  image_url: "https://static-content.alfagift.id/promo/assets/image/2024/juni/Slide Banner SunScreen.png",
+  direct_url: "https://alfagift.id/promotion-products?id=667aad845ffbfc6f870e5c40",
+  modal_content: {},
+},
+{ 
+  id: 2,
+  type: "modal",
+  image_url: "https://static-content.alfagift.id/promo/assets/image/2024/juni/Slide Banner Serum Renew.png",
+  direct_url: "",
+  modal_content: {
+    category: "restoran",
+    name: "SOES SURGAWI",
+    image: "FBtDlzHBg0kvt3EhG1LZ.jpg",
+    tnc: [
+      "<strong>Diskon 5%</strong> untuk semua menu dengan minimum transaksi Rp 50.000,-",
+      "Harus menunjukkan kartu member fisik atau kartu member digital keanggotaan A-Member",
+      "Tidak dapat digabungkan dengan promo lain",
+      "Promo ini berlaku dari 1 Juni 2024 s.d 30 November 2024",
+    ],
+    loc: "",
+    loclist : [
+      "Ruko Kebayoran Arcade 1 C3-37, Bintaro Sektor 7, Pondok Aren, Pondok Jaya - Tangerang Selatan",
+      "Jl Cipete Raya no.18, Fatmawati - Jakarta Selatan",
+      "Bintaro Xchange Mall 1, Lt B1, Pondok Aren, Pondok Jaya - Tangerang Selatan",
+    ],
+    filteredCity: "TANGERANG SELATAN",
+    reserve: [
+      "<strong>Telephone</strong> : 087878651224",
+      "<strong>Email</strong> : soessurgawi@gmail.com",
+      "<strong>Socmed</strong> : @soessurgawi",
+    ],
+    additional: [],
+    additionalnew: []
+  }
+},
+{ 
+  id: 3,
+  type: "page",
+  image_url: "https://static-content.alfagift.id/promo/assets/image/2024/juni/Slide Banner Vita Smoothies.png",
+  direct_url: "https://bakmigm.com",
+  modal_content: {},
+},
 ]
 
 export default Carousel;
--- src/components/Main.js
+++ src/components/Main.js
@@ -8,6 +8,18 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 const Main = () => {
+  const [countTotalMerchant, setCountTotalMerchant] = useState('');
+  const loadDataOnlyOnce = () => {
+    const totalData = Merchants.filter(data =>
+      data.category === 'penginapan'
+    );
+    console.log(totalData.length);
+    setCountTotalMerchant(totalData.length);
+  }
+  useEffect(() => {
+    loadDataOnlyOnce();
+  }, [])
+
   const [searchTerm, setSearchTerm] = useState('');
   const [results, setResults] = useState([]);
 
@@ -19,8 +31,8 @@ const Main = () => {
       setResults([]);
     } else {
       const filteredMerchants = Merchants.filter(list =>
-        list.name.toLowerCase().includes(value) ||
-        list.loc.toLowerCase().includes(value)
+        list.name.toLowerCase().includes(value)
+        // list.loc.toLowerCase().includes(value)
         // list.tnc.map(tc => tc.toLowerCase()).includes(value)
       );
 
@@ -48,6 +60,10 @@ const Main = () => {
   const handleClose = () => setShowModal(undefined);
   const handleShow = (merchantId) => setShowModal(merchantId);
 
+  const [showBanner, setShowBanner] = useState(false);
+  const modalBannerShow = () => setShowBanner(true);
+  const modalBannerClose = () => setShowBanner(false);
+
   const [cari, setCari] = useState(false);
   const cariClose = () => setCari(false);
   const cariShow = () => setCari(true);
@@ -58,25 +74,31 @@ const Main = () => {
   const filterClose = () => setFilter(false);
   const filterShow = () => setFilter(true);
   const [selectedLocation, setSelectedLocation] = useState('');
+  const [countSelectedLocation, setCountSelectedLocation] = useState('');
 
   const handleFilteredByLocation = (location) => {
     const filtered = Merchants.filter(list =>
-      list.loc.includes(location) ||
-      list.loclist.some(loc => loc.includes(location)) ||
-      list.filteredCity.toLowerCase().includes(location.toLowerCase())
+      list.filteredCity.toLowerCase().includes(location.toLowerCase()) &&
+      list.category === 'penginapan'
     );
-    console.log(location);
     const getLocation = location;
     setSelectedLocation(getLocation);
+    setCountSelectedLocation(filtered.length);
     
     if (filtered.length > 0) {
       setFilteredData(filtered);
       filterClose();
       document.getElementById('emptyStateFilterLocation').classList.add('d-none');
+      document.getElementById('counterData').classList.add('d-none');
+      document.getElementById('btnFilter').classList.add('d-none');
+      document.getElementById('filterLocResult').classList.remove('d-none');
     } else {
       setFilteredData(filtered);
       filterClose();
       document.getElementById('emptyStateFilterLocation').classList.remove('d-none');
+      document.getElementById('counterData').classList.add('d-none');
+      document.getElementById('filterLocResult').classList.add('d-none');
+      document.getElementById('btnFilter').classList.add('d-none');
     }
   };
 
@@ -94,15 +116,19 @@ const Main = () => {
   const handleReset = () => {
     setFilteredData(Merchants);
     filterClose();
+    document.getElementById('emptyStateFilterLocation').classList.add('d-none');
+    document.getElementById('counterData').classList.remove('d-none');
+    document.getElementById('filterLocResult').classList.add('d-none');
+    document.getElementById('btnFilter').classList.remove('d-none');
   }
 
   useEffect(() => {
     // console.log('Filtered Data:', filteredData);
   }, [filteredData]);
 
-  const settings = {
+  const sliderSettings = {
     dots: true,
-    infinite: false,
+    infinite: true,
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
@@ -117,7 +143,7 @@ const Main = () => {
             <div className='category-tab'>
               <ul className='nav nav-tabs'>
                 <li className='nav-item'>
-                  <div className='nav-link active'>Semua</div>
+                  <div className='nav-link active'>Penginapan & Rekreasi</div>
                 </li>
                 <li className='nav-item'>
                   <a href='https://alfagift.id/external-webview?title=Merchant&url=https://alfagift.id/resources/merchant/restoran/' className='nav-link'>Restoran</a>
@@ -126,48 +152,137 @@ const Main = () => {
                   <a href='https://alfagift.id/external-webview?title=Merchant&url=https://alfagift.id/resources/merchant/kesehatan-kecantikan/' className='nav-link'>Kesehatan & Kecantikan</a>
                 </li>
                 <li className='nav-item'>
-                  <a href='https://alfagift.id/external-webview?title=Merchant&url=https://alfagift.id/resources/merchant/penginapan-rekreasi/' className='nav-link'>Penginapan & Rekreasi</a>
-                </li>
-                <li className='nav-item'>
                   <a href='https://alfagift.id/external-webview?title=Merchant&url=https://alfagift.id/resources/merchant/fashion-lifestyle/' className='nav-link'>Fashion & Lifestyle</a>
                 </li>
                 <li>&nbsp;</li>
               </ul>
             </div>
             <div className='d-flex align-items-center pe-3 ps-2'>
-              <button type='button' className='btn btn-search me-1' style={{ color: '#666666' }} onClick={() => { cariShow(); onClear(); }}>
+              <button type='button' className='btn btn-search' style={{ color: '#666666' }} onClick={() => { cariShow(); onClear(); }}>
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-search" viewBox="0 0 16 16">
                   <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"/>
                 </svg>
               </button>
-              <button type='button' className='btn btn-search' style={{ color: '#666666' }} onClick={() => { filterShow(); onClear(); }}>
+              {/* <button type='button' className='btn btn-search' style={{ color: '#666666' }} onClick={() => { filterShow(); onClear(); }}>
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-funnel" viewBox="0 0 16 16">
                   <path d="M1.5 1.5A.5.5 0 0 1 2 1h12a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-.128.334L10 8.692V13.5a.5.5 0 0 1-.342.474l-3 1A.5.5 0 0 1 6 14.5V8.692L1.628 3.834A.5.5 0 0 1 1.5 3.5zm1 .5v1.308l4.372 4.858A.5.5 0 0 1 7 8.5v5.306l2-.666V8.5a.5.5 0 0 1 .128-.334L13.5 3.308V2z"/>
                 </svg>
-              </button>
+              </button> */}
             </div>
           </div>
         </div>
+        <Row>
+          <Col xs={12}>
+            <div className="slider-container mb-3">
+              <Slider {...sliderSettings}>
+                {Carousel.map((list) => (
+                  <div key={list.id}>
+                    { list.type === 'page' ? 
+                      <a href={list.direct_url} target='_blank' className='text-decoration-none'>
+                        <img src={list.image_url} alt='' style={{ width: "100%", borderRadius: "12px" }} />
+                      </a> : 
+                      list.type === 'modal' ? 
+                      <div>
+                        <div onClick={modalBannerShow}>
+                          <img src={list.image_url} alt='' style={{ width: "100%", borderRadius: "12px" }} />
+                        </div>
+                        <Modal
+                          size="lg"
+                          aria-labelledby="contained-modal-title-vcenter"
+                          centered
+                          show={showBanner}
+                          onHide={modalBannerClose}
+                          className='bottomsheet-halffull'
+                        >
+                          <Modal.Body className="px-0">
+                            <div className='row justify-content-center mx-0'>
+                              <div className='col-8 p-3'>
+                                <img src={'https://static-content.alfagift.id/static/merchant/' + list.modal_content.image} alt='' className='img-fluid' />
+                              </div>
+                            </div>
+                            <div className='divider'></div>
+                            <div className='p-3'>
+                              <div className='text-sm fw7 text-gray'>{ list.modal_content.category }</div>
+                              <div className='text-xlg fw7'>{ list.modal_content.name }</div>
+                            </div>
+                            <div className='px-3'>
+                              <div className='fw5 text-lg'>Syarat & Ketentuan</div>
+                              <ul className='list-content my-3'>
+                                {list.modal_content.tnc.map(terms => (
+                                  <li key={terms}>
+                                    <div dangerouslySetInnerHTML={{__html: terms}}></div>
+                                  </li>
+                                ))}
+                              </ul>
+                            </div>
+                            <div className='border-dashed my-3'></div>
+                            <div className='px-3'>
+                              {list.modal_content.loclist.length > 0 || list.modal_content.loc !== '' ? <div className='fw5 text-lg'>Promo berlaku di:</div> : null}
+                              {list.modal_content.loc !== '' ? <div className='my-3' dangerouslySetInnerHTML={{__html: list.modal_content.loc}}></div> : null}
+                              {list.modal_content.loclist.length > 0 ? 
+                                <ul className='list-content my-3'>
+                                  {list.modal_content.loclist.map(locations => (
+                                    <li key={locations}>
+                                      <div dangerouslySetInnerHTML={{__html: locations}}></div>
+                                    </li>
+                                  ))}
+                                </ul>
+                              : null}
+                            </div>
+                            <div className='border-dashed my-3'></div>
+                            <div className='px-3'>
+                              {list.modal_content.reserve.length > 0 ? 
+                                <div>
+                                  <div className='fw5 text-lg'>Informasi:</div>
+                                  <div className='my-3'>
+                                    {list.modal_content.reserve.map(info => (
+                                      <div key={info} className='mb-2'>
+                                        <div dangerouslySetInnerHTML={{__html: info}}></div>
+                                      </div>
+                                    ))}
+                                  </div>
+                                </div>
+                              : null}
+                            </div>
+                          </Modal.Body>
+                          <div className='fixed-action p-3'>
+                            <Button variant='outline-primary w-100' onClick={modalBannerClose}>Tutup</Button>
+                          </div>
+                        </Modal>
+                      </div> : null }
+                  </div>
+                ))}
+              </Slider>
+            </div>
+          </Col>
+        </Row>
         <div className='merchant-result'>
           <div className='result-list'>
-          <div className="slider-container">
-          <Slider {...settings}>
-            {Carousel.map((list) => (
-              <Col key={list.id} className='py-2'>
-              <div>
-                <img
-                  src={'https://static-content.alfagift.id/promo/assets/image/2025/januari/' + list.image}
-                  alt=''
-                  className='img-fluid'
-                />
-              </div>
-              </Col>
-            ))}
-          </Slider>
-          </div>
-            <div>Menampilkan semua kategori merchant di {selectedLocation}</div>
             <Row className='px-2'>
-              {filteredData.map(list => (
+              <Col xs={12} className='px-2'>
+                <div className='mb-2'>
+                  <h5 className='mb-2 fw7'>Penginapan & Rekreasi</h5>
+                  <div id='btnFilter' onClick={() => { filterShow(); onClear(); }} className='d-flex align-items-center' style={{ border: "solid 1px #D8D8D8", padding: "6px 8px", fontSize: "12px", borderRadius: "8px", width: "fit-content", backgroundColor: "#FFFFFF" }}>
+                    <span className='me-1'>Semua Lokasi</span>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" fill="currentColor" class="bi bi-chevron-down" viewBox="0 0 16 16">
+                      <path fill-rule="evenodd" d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708"/>
+                    </svg>
+                  </div>
+                  <div id='filterLocResult' className='d-none my-2'>
+                    <div onClick={() => { filterShow(); onClear(); }} className='d-flex align-items-center mb-2' style={{ color: "#005DA6", backgroundColor: "#F8FBFF", border: "solid 1px #005DA6", padding: "6px 8px", fontSize: "12px", borderRadius: "8px", width: "fit-content" }}>
+                      <span className='me-1'>{selectedLocation}</span>
+                      <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" fill="currentColor" class="bi bi-chevron-down" viewBox="0 0 16 16">
+                        <path fill-rule="evenodd" d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708"/>
+                      </svg>
+                    </div>
+                    <div className='d-flex align-items-center justify-content-between mt-3'>
+                      <div className='fw7' style={{ color: "#666666" }}>{countSelectedLocation} Merchant</div>
+                    </div>
+                  </div>
+                  <div id='counterData' className='fw7 mt-3' style={{ color: "#666666" }}>{countTotalMerchant} Merchant</div>
+                </div>
+              </Col>
+              {filteredData.filter((item) => item.category === "penginapan").map((list) => (
                 <Col key={list.merchantId} xs={6} sm={6} md={3} lg={2} className='px-1 py-2'>
                   <div className='card merc-card border-0' onClick={()=>handleShow(list.merchantId)}>
                     <img src={'https://static-content.alfagift.id/static/merchant/' + list.image} alt='' className='img-fluid' />
@@ -202,7 +317,7 @@ const Main = () => {
                         <div className='text-xlg fw7'>{name}</div>
                       </div>
                       <div className='px-3'>
-                      <div className='fw5 text-lg'>Syarat & Ketentuan:</div>
+                        <div className='fw5 text-lg'>Syarat & Ketentuan:</div>
                         <ul className='list-content my-3'>
                           {tnc.map(terms => (
                             <li key={terms}>
@@ -254,8 +369,9 @@ const Main = () => {
               <Col xs={8}>
                 <img src='https://static-content.alfagift.id/static/alfagift-app/no_search_result.png' alt='' className='img-fluid mb-3' />
               </Col>
-              <Col xs={10}>
-                <p className='text-center'>Tidak ada merchant di "{selectedLocation}"</p>
+              <Col xs={10} className='text-center'>
+                <p className='text-center'>Tidak ditemukan penginapan di "<strong style={{ textTransform: "uppercase" }}>{selectedLocation}</strong>"</p>
+                <span onClick={() => { filterShow(); onClear(); }} style={{ fontSize: "14px", fontWeight: "bold", color: "#005DA6" }}>Ubah Lokasi</span>
               </Col>
             </Row>
           </div>

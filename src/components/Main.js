7e68bed8fb642cf189c6c20bcee686b54f273acc
Bagus Arisandi <bagus.arisandi@gli.id>
2025-01-09 13:24:40 +0700

    merchants

--- src/components/Main.js
+++ src/components/Main.js
@@ -139,13 +139,13 @@ const Main = () => {
     <div>
       <Container className='content'>
         <div className='navigator'>
-          <div className='px-3 mb-2'>
-            <button type='button' className='btn btn-search d-flex align-items-center w-100' onClick={() => { cariShow(); onClear(); }}>
+          <div className='px-3 mb-3'>
+            <div className='btn-search d-flex align-items-center shadow-sm w-100' onClick={() => { cariShow(); onClear(); }}>
               <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-search" viewBox="0 0 16 16">
                 <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"/>
               </svg>
               <div style={{ color: "#999999", fontSize: "14px" }}>Cari merchant di semua kategori</div>
-            </button>
+            </div>
           </div>
           <div className='category-tab'>
             <ul className='nav nav-tabs'>

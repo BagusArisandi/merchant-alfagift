9f3f85a5270177c2115de4d35f86fde556bd8996
Bagus Arisandi <bagus.arisandi@gli.id>
2025-01-09 11:38:06 +0700

    update merch staging

--- src/components/Main.js
+++ src/components/Main.js
@@ -83,8 +83,6 @@ const Main = () => {
     const getLocation = location;
     setSelectedLocation(getLocation);
     setCountSelectedLocation(filtered.length);
-
-    console.log(location);
     
     if (filtered.length > 0) {
       setFilteredData(filtered);
@@ -141,36 +139,30 @@ const Main = () => {
     <div>
       <Container className='content'>
         <div className='navigator'>
-          <div className='d-flex align-items-center'>
-            <div className='category-tab'>
-              <ul className='nav nav-tabs'>
-                <li className='nav-item'>
-                  <div className='nav-link active'>Penginapan & Rekreasi</div>
-                </li>
-                <li className='nav-item'>
-                  <a href='https://alfagift.id/external-webview?title=Merchant&url=https://alfagift.id/resources/merchant/restoran/' className='nav-link'>Restoran</a>
-                </li>
-                <li className='nav-item'>
-                  <a href='https://alfagift.id/external-webview?title=Merchant&url=https://alfagift.id/resources/merchant/kesehatan-kecantikan/' className='nav-link'>Kesehatan & Kecantikan</a>
-                </li>
-                <li className='nav-item'>
-                  <a href='https://alfagift.id/external-webview?title=Merchant&url=https://alfagift.id/resources/merchant/fashion-lifestyle/' className='nav-link'>Fashion & Lifestyle</a>
-                </li>
-                <li>&nbsp;</li>
-              </ul>
-            </div>
-            <div className='d-flex align-items-center pe-3 ps-2'>
-              <button type='button' className='btn btn-search' style={{ color: '#666666' }} onClick={() => { cariShow(); onClear(); }}>
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-search" viewBox="0 0 16 16">
-                  <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"/>
-                </svg>
-              </button>
-              {/* <button type='button' className='btn btn-search' style={{ color: '#666666' }} onClick={() => { filterShow(); onClear(); }}>
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-funnel" viewBox="0 0 16 16">
-                  <path d="M1.5 1.5A.5.5 0 0 1 2 1h12a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-.128.334L10 8.692V13.5a.5.5 0 0 1-.342.474l-3 1A.5.5 0 0 1 6 14.5V8.692L1.628 3.834A.5.5 0 0 1 1.5 3.5zm1 .5v1.308l4.372 4.858A.5.5 0 0 1 7 8.5v5.306l2-.666V8.5a.5.5 0 0 1 .128-.334L13.5 3.308V2z"/>
-                </svg>
-              </button> */}
-            </div>
+          <div className='px-3 mb-2'>
+            <button type='button' className='btn btn-search d-flex align-items-center w-100' onClick={() => { cariShow(); onClear(); }}>
+              <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-search" viewBox="0 0 16 16">
+                <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"/>
+              </svg>
+              <div style={{ color: "#999999", fontSize: "14px" }}>Cari merchant di semua kategori</div>
+            </button>
+          </div>
+          <div className='category-tab'>
+            <ul className='nav nav-tabs'>
+              <li className='nav-item'>
+                <div className='nav-link active'>Penginapan & Rekreasi</div>
+              </li>
+              <li className='nav-item'>
+                <a href='https://alfagift.id/external-webview?title=Merchant&url=https://alfagift.id/resources/merchant/restoran/' className='nav-link'>Restoran</a>
+              </li>
+              <li className='nav-item'>
+                <a href='https://alfagift.id/external-webview?title=Merchant&url=https://alfagift.id/resources/merchant/kesehatan-kecantikan/' className='nav-link'>Kesehatan & Kecantikan</a>
+              </li>
+              <li className='nav-item'>
+                <a href='https://alfagift.id/external-webview?title=Merchant&url=https://alfagift.id/resources/merchant/fashion-lifestyle/' className='nav-link'>Fashion & Lifestyle</a>
+              </li>
+              <li><div style={{ width: "12px" }}></div></li>
+            </ul>
           </div>
         </div>
         <Row>

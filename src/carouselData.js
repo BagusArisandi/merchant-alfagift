36e2ec7f368d661b72050f25fcf1f53b44b72824
Bagus Arisandi <bagus.arisandi@gli.id>
2025-02-25 13:14:50 +0700

    reposition merchant counter

--- src/carouselData.js
+++ src/carouselData.js
@@ -1,46 +1,22 @@
 const Carousel = [
-  // {
-  //   id: 1,
-  //   type: "page",
-  //   image_url: "https://static-content.alfagift.id/promo/assets/image/2024/juni/Slide Banner SunScreen.png",
-  //   direct_url: "https://alfagift.id/promotion-products?id=667aad845ffbfc6f870e5c40",
-  // },
-  // {
-  //   id: 2,
-  //   type: "modal",
-  //   image_url: "https://static-content.alfagift.id/promo/assets/image/2024/juni/Slide Banner Serum Renew.png",
-  //   direct_url: "",
-  // },
+  {
+    id: 1,
+    type: "page",
+    image_url: "https://static-content.alfagift.id/static/merchant/7d85c071-a5fe-4ebc-b2d1-9d8a7f169e47.jpeg",
+    direct_url: "https://alfagift.id/static_page?id=67af0f4d52263f14128c9274&name=promo_surabaya",
+  },
+  {
+    id: 2,
+    type: "modal",
+    image_url: "https://static-content.alfagift.id/static/merchant/15e35d53-3911-45ca-8547-2353e5da5eed.jpeg",
+    direct_url: "591",
+  },
   {
     id: 3,
     type: "modal",
-    image_url: "https://static-content.alfagift.id/promo/assets/image/2024/juni/Slide Banner Vita Smoothies.png",
-    direct_url: "74",
+    image_url: "https://static-content.alfagift.id/static/merchant/78e4b02d-9b59-47d1-a4f0-ecd94bba3cb2.jpeg",
+    direct_url: "636",
   },
-  // {
-  //   id: 4,
-  //   type: "static",
-  //   image_url: "https://static-content.alfagift.id/static/merchant/image_placeholder.png",
-  //   direct_url: "",
-  // },
-  {
-    id: 5,
-    type: "modal",
-    image_url: "https://static-content.alfagift.id/promo/assets/image/2024/juni/Slide Banner Serum Renew.png",
-    direct_url: "478",
-  },
-  {
-    id: 6,
-    type: "modal",
-    image_url: "https://static-content.alfagift.id/promo/assets/image/2024/juni/Slide Banner Vita Smoothies.png",
-    direct_url: "605",
-  },
-  // {
-  //   id: 7,
-  //   type: "modal",
-  //   image_url: "https://static-content.alfagift.id/promo/assets/image/2024/juni/Slide Banner Vita Smoothies.png",
-  //   direct_url: "599",
-  // },
 ]
 
 export default Carousel;
